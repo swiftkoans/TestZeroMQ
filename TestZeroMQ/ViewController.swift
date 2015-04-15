5568bc0d7b6a2e63cd8134d59bd6c7018271eaf7
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-15 15:54:39 +0800

    Initial Commit

---
+++ TestZeroMQ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestZeroMQ
+//
+//  Created by Calvin Cheng on 15/4/15.
+//  Copyright (c) 2015 Algo Access Pte. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
