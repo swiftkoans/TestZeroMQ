f7a6558487cb174f81760e4d5f2a790ae574a60d
Calvin Cheng <linchuan.cheng@gmail.com>
2015-04-24 16:55:48 +0800

    Swift-to-CPP bridging header.

---
+++ TestZeroMQ/TestZeroMQ-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  TestZeroMQ-Bridging-Header.h
+//  TestZeroMQ
+//
+//  Created by Calvin Cheng on 24/4/15.
+//  Copyright (c) 2015 Algo Access Pte. Ltd. All rights reserved.
+//
+
+#ifndef TestZeroMQ_TestZeroMQ_Bridging_Header_h
+#define TestZeroMQ_TestZeroMQ_Bridging_Header_h
+
+#import "zmq_utils.h"
+#import "zmq.h"
+
+#endif
